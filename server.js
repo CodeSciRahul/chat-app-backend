e4aa3e69bde897469b829bf3a043546bb863ab25
= <=>
2025-01-13 16:50:37 +0530

    Bug: resolve CORS Error

--- server.js
+++ server.js
@@ -27,22 +27,20 @@ const app = express();
 
 const server = http.createServer(app);
 export const io = new Server(server, {
-  cors: { origin: "http://localhost:5173", methods: ["GET", "POST"] },
+  cors: { 
+    origin: "*", // Allow all origins
+    methods: ["GET", "POST"], 
+    allowedHeaders: ["Content-Type", "Authorization"]
+  },
   pingInterval: 25000, // 25 seconds
   pingTimeout: 10000, // 10 seconds
   maxHttpBufferSize: 1e8, // Increase the max buffer size for large files (100MB)
 });
 
+
 // Updated CORS configuration
 const corsOptions = {
-  origin: (origin, callback) => {
-    const allowedOrigins = ["http://localhost:5173"];
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: "*", // Allow all origins
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
--- src/config/properties.js
+++ src/config/properties.js
@@ -9,14 +9,12 @@ const properties = {
     CLIENT_URL: process.env.Frontend_url || `http://localhost:5173`,
   
     MONGO_URL:
-      process.env.MONGO_URL || `mongodb://localhost:27017/chat-app`,
+      process.env.MONGO_URL || `mongodb+srv://rahulkumarkudra2004:SeIvbsEzdnpRKFN3@cluster0.ty7ym.mongodb.net`,
     
     SALT_ROUND: Number(process.env.SALT_ROUND) || 10,
 
     SECERT_KEY: process.env.SECRET_KEY || "rahulkumar@1234",
     EMAIL_VERIFICATION_SECERT_KEY: process.env.Email_Verification_Secret_key || "himanshinehakhushi@787007183"
-
-  
   };
   
   export default properties;
